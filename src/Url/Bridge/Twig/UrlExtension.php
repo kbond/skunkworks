e4cd9472dad62546403c90a5d33cdbb358865f9d
Kevin Bond <kevinbond@gmail.com>
2020-11-18 11:33:14 -0500

    [bug][url] remove twig functions as url() conflicts with Symfony's url()

--- src/Url/Bridge/Twig/UrlExtension.php
+++ src/Url/Bridge/Twig/UrlExtension.php
@@ -4,7 +4,6 @@ namespace Zenstruck\Url\Bridge\Twig;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
-use Twig\TwigFunction;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
@@ -16,14 +15,6 @@ final class UrlExtension extends AbstractExtension
         return [
             new TwigFilter('url', 'Zenstruck\url'),
             new TwigFilter('mailto', 'Zenstruck\mailto'),
-        ];
-    }
-
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction('url', 'Zenstruck\url'),
-            new TwigFunction('mailto', 'Zenstruck\mailto'),
         ];
     }
 }
