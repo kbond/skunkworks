7d08cf4c2c0764764e19373301e1c8cdfa46b243
Kevin Bond <kevinbond@gmail.com>
2022-11-01 15:56:55 -0400

    add DotAccessor

---
+++ src/Utilities/DotAccessor.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Zenstruck\Utilities;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+final class DotAccessor
+{
+    public function __construct(private array|object $value)
+    {
+    }
+
+    /**
+     * Get an item using "dot" notation.
+     *
+     * @author Taylor Otwell <taylor@laravel.com>
+     * @source https://github.com/laravel/framework/blob/e9483c441d5f0c8598d438d6024db8b1a7aa55fe/src/Illuminate/Collections/Arr.php#L286
+     *
+     * @template T
+     *
+     * @param T $default
+     *
+     * @return T $default if no match
+     */
+    public function get(string $path, mixed $default = null): mixed
+    {
+        // first check if path exists on array
+        if (\is_array($this->value) && \array_key_exists($path, $this->value)) {
+            return $this->value[$path];
+        }
+
+        if (\is_array($this->value)) {
+            $current = &$this->value;
+        } else {
+            $current = $this->value;
+        }
+
+        foreach (\explode('.', $path) as $segment) {
+            if (\is_array($current) && \array_key_exists($segment, $current)) {
+                $current = &$current[$segment];
+
+                continue;
+            }
+
+            if (!\is_object($current)) {
+                return $default;
+            }
+
+            if (\property_exists($current, $segment)) {
+                $current = &$current->$segment;
+
+                continue;
+            }
+
+            if (\method_exists($current, $segment)) {
+                $current = $current->$segment();
+
+                continue;
+            }
+
+            foreach (['get', 'has', 'is'] as $prefix) {
+                if (\method_exists($current, $method = $prefix.\ucfirst($segment))) {
+                    $current = $current->$method();
+
+                    continue 2;
+                }
+            }
+
+            return $default;
+        }
+
+        return $current;
+    }
+
+    /**
+     * Check if an item or items exist using "dot" notation.
+     */
+    public function has(string ...$paths): bool
+    {
+        if (!$paths) {
+            return false;
+        }
+
+        foreach ($paths as $path) {
+            if ('__DEFAULT__' === $this->get($path, '__DEFAULT__')) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ tests/Utilities/Tests/DotAccessorTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Zenstruck\Utilities\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zenstruck\Utilities\DotAccessor;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+final class DotAccessorTest extends TestCase
+{
+    /**
+     * @author Taylor Otwell <taylor@laravel.com>
+     * @source https://github.com/laravel/framework/blob/56f79e2b6e4ee99b26be0c5c73d57cd3a3974fd1/tests/Support/SupportArrTest.php#L249
+     *
+     * @test
+     * @dataProvider getProvider
+     */
+    public function can_get($what, $path, $expected, $default = null): void
+    {
+        $this->assertSame($expected, (new DotAccessor($what))->get($path, $default));
+    }
+
+    public static function getProvider(): iterable
+    {
+        yield [['products.desk' => ['price' => 100]], 'products.desk', ['price' => 100]];
+        yield [['products' => ['desk' => ['price' => 100]]], 'products.desk', ['price' => 100]];
+        yield [['products' => ['desk' => ['price' => 100]]], 'products.desk.price', 100];
+        yield [['foo' => null, 'bar' => ['baz' => null]], 'foo', null, 'default'];
+        yield [['foo' => null, 'bar' => ['baz' => null]], 'bar.baz', null, 'default'];
+        yield [['products' => [['name' => 'desk'], ['name' => 'chair']]], 'products.0.name', 'desk'];
+        yield [['products' => [['name' => 'desk'], ['name' => 'chair']]], 'products.1.name', 'chair'];
+        yield [['names' => ['developer' => 'taylor']], 'names.otherDeveloper', 'dayle', 'dayle'];
+        yield [['obj' => SomeObject::create()], 'obj.prop1', 'a'];
+        yield [['obj' => SomeObject::create()], 'obj.method1', 'c'];
+        yield [['obj' => SomeObject::create()], 'obj.method2', 'd'];
+        yield [['obj' => SomeObject::create()], 'obj.method3', 'e'];
+        yield [['obj' => SomeObject::create()], 'obj.method4', 'f'];
+        yield [SomeObject::create(), 'prop1', 'a'];
+        yield [SomeObject::create(), 'method1', 'c'];
+        yield [SomeObject::create(), 'method2', 'd'];
+        yield [SomeObject::create(), 'method3', 'e'];
+        yield [SomeObject::create(), 'method4', 'f'];
+        yield [SomeObject::create(), 'prop2.v1', 'b'];
+        yield [SomeObject::create(), 'prop2.self.prop1', 'a'];
+        yield [SomeObject::create(), 'prop2.self.method5.prop1', 'a'];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.method5.prop1', 'a'];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.method5.invalid', null];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.invalid.prop1', null];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.method5.invalid', 'default', 'default'];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.invalid.prop1', 'default', 'default'];
+    }
+
+    /**
+     * @test
+     * @dataProvider hasProvider
+     */
+    public function can_has($what, $path, $expected): void
+    {
+        $this->assertSame($expected, (new DotAccessor($what))->has(...(array) $path));
+    }
+
+    public static function hasProvider(): iterable
+    {
+        yield [['products.desk' => ['price' => 100]], 'products.desk', true];
+        yield [['products' => ['desk' => ['price' => 100]]], 'products.desk', true];
+        yield [['products' => ['desk' => ['price' => 100]]], 'products.desk.price', true];
+        yield [['foo' => null, 'bar' => ['baz' => null]], 'foo', true];
+        yield [['foo' => null, 'bar' => ['baz' => null]], 'bar.baz', true];
+        yield [['products' => [['name' => 'desk'], ['name' => 'chair']]], 'products.0.name', true];
+        yield [['products' => [['name' => 'desk'], ['name' => 'chair']]], 'products.1.name', true];
+        yield [['names' => ['developer' => 'taylor']], 'names.otherDeveloper', false];
+        yield [['obj' => SomeObject::create()], 'obj.prop1', true];
+        yield [['obj' => SomeObject::create()], 'obj.method1', true];
+        yield [['obj' => SomeObject::create()], 'obj.method2', true];
+        yield [['obj' => SomeObject::create()], 'obj.method3', true];
+        yield [['obj' => SomeObject::create()], 'obj.method4', true];
+        yield [SomeObject::create(), ['prop1', 'method1'], true];
+        yield [SomeObject::create(), ['prop1', 'invalid'], false];
+        yield [SomeObject::create(), 'prop1', true];
+        yield [SomeObject::create(), 'method1', true];
+        yield [SomeObject::create(), 'method2', true];
+        yield [SomeObject::create(), 'method3', true];
+        yield [SomeObject::create(), 'method4', true];
+        yield [SomeObject::create(), 'prop2.v1', true];
+        yield [SomeObject::create(), 'prop2.self.prop1', true];
+        yield [SomeObject::create(), 'prop2.self.method5.prop1', true];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.method5.prop1', true];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.method5.invalid', false];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.invalid.prop1', false];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.method5.invalid', false];
+        yield [['obj' => SomeObject::create()], 'obj.prop2.self.invalid.prop1', false];
+    }
+}
+
+class SomeObject
+{
+    public $prop1 = 'a';
+    public $prop2 = ['v1' => 'b'];
+
+    public static function create(): self
+    {
+        $object = new self();
+        $object->prop2['self'] = new self();
+
+        return $object;
+    }
+
+    public function method1(): string
+    {
+        return 'c';
+    }
+
+    public function getMethod2(): string
+    {
+        return 'd';
+    }
+
+    public function hasMethod3(): string
+    {
+        return 'e';
+    }
+
+    public function isMethod4(): string
+    {
+        return 'f';
+    }
+
+    public function method5(): self
+    {
+        return new self();
+    }
+}
